b67d0630bb90dbb40ef7507fa1a0f8940d1ed6e0
ajithbackyard <56559326+ajithbackyard@users.noreply.github.com>
2021-06-08 15:16:48 +0530

    Update flex.ino

--- flex.ino
+++ flex.ino
@@ -3,7 +3,7 @@ Servo servo1;
 const int flexpin = A0; 
 void setup () 
 { 
- Serial.begin(115200); 
+ Serial.begin(9600); 
  servo1.attach(9); 
 } 
 void loop () 
@@ -12,7 +12,7 @@ void loop ()
  flexposition = analogRead(flexpin);
  Serial.print("sensor: "); 
  Serial.print(flexposition); 
- if (flexposition > 800)
+ if (flexposition > 800)// you can change this value according to your sensor reading
  {
   servo1.write(0);
   Serial.print(" servo:"); 
